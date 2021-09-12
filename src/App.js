08769c5ab8e299e3edbafb0d7d73bbe363aa15bd
ji_0kim <jiyoung26091212@gmail.com>
2021-09-12 18:11:43 +0100

    component life cycle

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import PropTypes from 'prop-types';
 
 
 class App extends React.Component{
-  // return 이 없다. React.component에 render method를 가져온다.
-  // class-는 클래스인데 보여주기 위해서는 render method안에 넣어줘야한다. 리엑트는 자동으로 class component안에 render method를 자동으로 렌더링한다.
+  // return 이 없다. React.component에 render method를 가져온다. class-는 클래스인데 보여주기 위해서는 render method안에 넣어줘야한다. 리엑트는 자동으로 class component안에 render method를 자동으로 렌더링한다
+
   state = {
     //변하는 state는 여기에 넣는다. 바꾸고싶은 데이터를 넣는거다.
     count: 0
+    
   }
-  add = (current) => {
-    console.log('add');
+
+  add = () => {
     // this.setState({ count: this.state.count + 1 });
     this.setState(current => ({ count: current.count + 1 }));
   }
+
   minus = () => { 
-    console.log('minus');
     // this.setState({ count: this.state.count - 1})
     this.setState(current => ({ count: current.count - 1 }));
     // render 가 다시 호출되어야한다. 그래서 'set'state. setState은 새로운 state으로 render를 호출한다.
     // current 사용하기
   }
+  componentDidMount(){
+    console.log('component rendered');
+  }
 
+  componentDidUpdate(){
+    console.log('I just updated');
+  }
 
+  componemtWillUnmount(){
+    console.log('good bye');
+  }
   render() {
+    console.log('i am rendering')
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
@@ -32,6 +43,11 @@ class App extends React.Component{
       </div>
     )
   }
+  // react component does
+  // mouting - being born
+  // Updating - updating
+  // Unmounting- when component dies. when page changes
+  // construnctor - con
 }
 
 export default App;
